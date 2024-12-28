dfb79f61c1c72db05aa0f00c26b1eadd64ae00f0
fzsf163 <yofaisol619@gmail.com>
2024-12-29 03:37:37 +0600

    dynamic url

--- app/components/AdminNav/primaryNav.tsx
+++ app/components/AdminNav/primaryNav.tsx
@@ -1,16 +1,26 @@
 import { Tooltip } from "@nextui-org/react";
 import { IconLogout } from "@tabler/icons-react";
-import { Form } from "react-router";
+import { Form, Link, useLocation } from "react-router";
 import ThemeToggler from "../themeSwitch";
 import Sidenav from "./sidenav";
 export default function PrimaryNav() {
+  const location = useLocation();
+  const path_name = location.pathname;
+
+  // Extract the last segment of the pathname
+  const segments = path_name.split("/").filter((segment) => segment !== "");
+  const lastSegment = segments[segments.length - 1];
+
+  // Construct the path to the last segment
+  const lastSegmentPath = `/${segments.slice(0, segments.length - 1).join("/")}`;
   return (
     <div className="sticky top-0 z-50 flex h-[4rem] items-center justify-around gap-4 bg-white/30 backdrop-blur-sm dark:bg-neutral-600/10">
       <div>
         <Sidenav></Sidenav>
       </div>
-      <h1 className="font-bold text-slate-600 dark:text-slate-200 sm:text-2xl">
-        Admin Panel
+      <h1 className="animate-appearance-in font-bold text-slate-600 dark:text-slate-200 sm:text-2xl">
+        Admin Panel +{" "}
+        <Link to={`${lastSegmentPath}/${lastSegment}`}>{lastSegment}</Link>
       </h1>
       <div className="flex items-center justify-center gap-4">
         <ThemeToggler></ThemeToggler>
