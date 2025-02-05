bf0943e58faf4946aad42999fd60b5b29ec05caf
fzsf163 <yofaisol619@gmail.com>
2025-02-05 23:01:18 +0600

    Add: time of creation

--- app/utils/converte_to_trending.ts
+++ app/utils/converte_to_trending.ts
@@ -1,14 +1,13 @@
 import { Post } from "~/types/post_data_type";
 import { Trending, TrendingBlogs } from "~/types/trending_types";
 
-export function convertPostDataToTrendingArray(
-  post: Post,
-): TrendingBlogs {
+export function convertPostDataToTrendingArray(post: Post): TrendingBlogs {
   const t: Trending[] = post.data.map((item) => ({
     id: item.id,
     img: item.thumbnail,
     readingTime: item.readTime,
     title: item.title,
+    published: item.createdAt ?? "Recent",
   }));
 
   return { trending: t ?? [] };
